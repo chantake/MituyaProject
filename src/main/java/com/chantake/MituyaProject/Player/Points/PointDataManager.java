e190e61e401ca4ac7230ed7d7a6baedebc97465b
ezura573 <>
2015-02-20 06:56:05 +0900

    ポイントデータ操作用クラスを新規作成

---
+++ src/main/java/com/chantake/MituyaProject/Player/Points/PointDataManager.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chantake.MituyaProject.Player.Points;
+
+
+import com.chantake.MituyaProject.Player.PlayerInstance;
+
+/**
+ *
+ * @author ezura
+ */
+public class PointDataManager {
+    
+    
+    public static void InsertPointLog(){
+    }
+    
+    
+    public static void InvestDailyPoint(PlayerInstance pi){    
+    }
+    
+    
+    
+    
+}
