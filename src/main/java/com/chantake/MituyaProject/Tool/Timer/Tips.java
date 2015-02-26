4354c0c397a830edc0bf31c50e1f30514971d20b
fumitti <fumitti@gmail.com>
2015-02-27 00:47:40 +0900

    add #53

--- src/main/java/com/chantake/MituyaProject/Tool/Timer/Tips.java
+++ src/main/java/com/chantake/MituyaProject/Tool/Timer/Tips.java
@@ -17,13 +17,12 @@
  */
 package com.chantake.MituyaProject.Tool.Timer;
 
-import java.util.ArrayList;
-
-import org.bukkit.ChatColor;
-
 import com.chantake.MituyaProject.MituyaProject;
 import com.chantake.MituyaProject.Tool.Tools;
+import org.bukkit.ChatColor;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -37,15 +36,23 @@ public final class Tips extends BukkitRunnable {
 
     public Tips(MituyaProject mp) {
         plugin = mp;
-        tip.add("放置はnew_worldのデフォルトリスポで！(ダメージ無効＆餓死しないよ！)");
-        tip.add("死んだ時は /spawn back で取りにいこう!!");
+        //tip.add("放置はnew_worldのデフォルトリスポで！(ダメージ無効＆餓死しないよ！)");
+        tip.add("死んだ時は /spawn back で死亡地点にテレポート！アイテムロストもこれで(一部)安心！");
+        tip.add("日本語MOD未導入時でも /ime on で変換機能を利用可能！");
+        tip.add("只今の時刻は <realtime> です");
+        tip.add("ここの場所にあとで来たい…そんな時は /save location で保存して /tp location ！");
+        tip.add("ログイン時の定型文は /save lm [設定したいメッセージ] で変更可能！");
+        tip.add("只今の時刻は <realtime> です");
+        tip.add("死亡時のデッドコメントは /save dm [設定したいメッセージ] で変更可能！");
         tip.add("チェストショップを有効活用しよう！");
-        //tip.add("動的マップを活用しよう！ (http://map.328mss.com/)");
+        tip.add("只今の時刻は <realtime> です");
+        tip.add("動的マップを活用しよう！ (http://map.328mss.com/)");
         tip.add("ホームを設定しましょう (/home set)");
-        tip.add("不具合報告などはフォーラムへ　http://forum.328mss.com/forums/");
+        tip.add("只今の時刻は <realtime> です");
+        //tip.add("不具合報告などはフォーラムへ　http://forum.328mss.com/forums/");
         tip.add("Wikiの編集大歓迎！自分の町の情報を載せちゃったり！？　http://wiki.328mss.com/");
         tip.add("ブロックを掘って ジャックポットを当てよう！！ (/jackpot)");
-        tip.add("不具合等ありましたらこちらに登録してチケットを発行してください http://redmine.328mss.com/");
+        //tip.add("不具合等ありましたらこちらに登録してチケットを発行してください http://redmine.328mss.com/");
         //tip.add("毎週月曜 24:00 ~ 26:00 は定期メンテナンスです");
         //tip.add("328一周年企画のアンケート実施中！328HPﾌｫｰﾗﾑの[ｲﾍﾞﾝﾄ企画,宣伝]のｲﾍﾞﾝﾄﾄﾋﾟｯｸまで！");
         //tip.add("みつやプラグイン漢字完全対応！！");
